163e8080643f375b5c6f4313f38504d7296fc9b0
M Ridwan <ridwanpratama9984@gmail.com>
2023-10-23 11:28:04 +0700

    responsive grid system, nav & ads slideshow

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MovieFlix</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/grid.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header class="row">
+        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
+            <a href="" class="logo">MovieFlix</a>
+        </div>
+        <nav class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 nav">
+            <ul class="nav-list">
+                <li class="nav-item">Home</li>
+                <li class="nav-item">Now Playing</li>
+                <li class="nav-item">Upcoming</li>
+                <li class="nav-item">Popular</li>
+                <li class="nav-item">Theaters</li>
+            </ul>
+        </nav>
+        <div class="col-md-4 col-lg-4 col-xl-4 search">
+            <input type="text">
+            <button>Search</button>
+        </div>
+    </header>
+
+    <main class="container">
+        <div class="row">
+            <div class="col-12 slideshow-container">
+                <div class="slides fade">
+                    <img src="assets/img/img_mountains_wide.jpg">
+                </div>
+                <div class="slides fade">
+                    <img src="assets/img/img_nature_wide.jpg">
+                </div>
+                <div class="slides fade">
+                    <img src="assets/img/img_snow_wide.jpg">
+                </div>
+            </div>
+            <br>
+            <div class="col-12 airing">
+                <h1>Airing Today</h1>
+            </div>
+        </div>
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
