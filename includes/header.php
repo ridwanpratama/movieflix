5433c8ebcb4bcbb20651a950f8e79418822b2ca5
M Ridwan <ridwanpratama9984@gmail.com>
2023-11-06 15:31:50 +0700

    change home

---
+++ includes/header.php
@@ -0,0 +1,73 @@
+<div class="h-overlay">
+</div>
+<header>
+    <nav class="nav-wrapper">
+        <ul class="nav-list">
+            <li class="nav-item">Home</li>
+            <li class="nav-item">Now Playing</li>
+            <li class="nav-item">Popular</li>
+            <li class="nav-item">Theaters</li>
+        </ul>
+    </nav>
+    <div>
+        <a href="" class="logo">MovieFlix</a>
+    </div>
+    <div class="search">
+        <select name="" id="" class="select-teater">
+            <option value="">Playing In - Jakarta</option>
+        </select>
+        <button>Change</button>
+        <ul class="nav-list">
+            <li class="nav-item my-account"><i class="fa fa-user"></i>&nbsp;My Account</li>
+        </ul>
+    </div>
+</header>
+<div class="row">
+    <div class="col-12 slideshow-container">
+        <div class="slides fade">
+            <div class="slide-content">
+               <div>
+                    <h2>This is the movie title</h2>
+               </div>
+               <div class="slide-info">
+                    <span class="uppercase">Documentary . 2h 49m</span>
+               </div>
+               <div class="slide-button">
+                    <button class="btn btn-book">Book Now</button>
+                    <button class="btn btn-watch">Watch Trailer</button>
+               </div>
+            </div>
+            <img src="assets/img/img_mountains_wide.jpg" alt="slide promo">
+        </div>       
+        <div class="slides fade">
+            <div class="slide-content">
+               <div>
+                    <h2>This is the movie title</h2>
+               </div>
+               <div class="slide-info">
+                    <span class="uppercase">Documentary . 2h 49m</span>
+               </div>
+               <div class="slide-button">
+                    <button class="btn btn-book">Book Now</button>
+                    <button class="btn btn-watch">Watch Trailer</button>
+               </div>
+            </div>
+            <img src="assets/img/img_snow_wide.jpg" alt="slide promo">
+        </div>    
+        <div class="slides fade">
+            <div class="slide-content">
+               <div>
+                    <h2>This is the movie title</h2>
+               </div>
+               <div class="slide-info">
+                    <span class="uppercase">Documentary . 2h 49m</span>
+               </div>
+               <div class="slide-button">
+                    <button class="btn btn-book">Book Now</button>
+                    <button class="btn btn-watch">Watch Trailer</button>
+               </div>
+            </div>
+            <img src="assets/img/img_nature_wide.jpg" alt="slide promo">
+        </div>    
+    </div>
+</div>
--- index.php
+++ index.php
@@ -5,134 +5,79 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieFlix</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="assets/css/grid.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
-
 <body>
-    <header class="row">
-        <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
-            <a href="" class="logo">MovieFlix</a>
-        </div>
-        <nav class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 nav">
-            <ul class="nav-list">
-                <li class="nav-item">Home</li>
-                <li class="nav-item">Now Playing</li>
-                <li class="nav-item">Popular</li>
-                <li class="nav-item">Theaters</li>
-            </ul>
-        </nav>
-        <div class="col-md-4 col-lg-4 col-xl-4 search">
-            <input type="text">
-            <button>Search</button>
-            <ul class="nav-list">
-                <li class="nav-item my-account"><i class="fa fa-user"></i>&nbsp;My Account</li>
-            </ul>
-        </div>
-    </header>
-    <main class="container">
-        <div class="row">
-            <div class="col-12 slideshow-container">
-                <div class="slides fade">
-                    <img src="assets/img/img_mountains_wide.jpg">
-                </div>
-                <div class="slides fade">
-                    <img src="assets/img/img_nature_wide.jpg">
-                </div>
-                <div class="slides fade">
-                    <img src="assets/img/img_snow_wide.jpg">
-                </div>
-            </div>
-            <br>
-        </div>
-        <div class="row">
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-4 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-4 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-4 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-4 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-4 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
-            </div>
-            <div class="col-lg-4 col-xl-2 col-md-4 col-sm-4 col-xs-6 movie airing">
-                <div class="movie-card">
-                    <img src="assets/img/movie1.jpg" alt="Movie Title">
-                    <h2>Movie Title</h2>
-                </div>
+<?php include 'includes/header.php'; ?>
+
+<section class="main-search">
+    <div class="main-search-wrapper">
+        <input type="text" class="main-search-input" placeholder="Search Movie, Cinema, City...">
+        <button class="main-search-button">Search</button>
+    </div>    
+</section>
+
+<!-- <main class="container">
+    <div class="row">
+        <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
+            <div class="movie-card">
+                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <h2><a href="pages/movie-detail.php">Movie Title</a></h2>
             </div>
         </div>
-        </div>
-    </main>
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <p>&copy; 2023 MovieFlix</p>
-                </div>
+        <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
+            <div class="movie-card">
+                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <h2><a href="pages/movie-detail.php">Movie Title</a></h2>
             </div>
         </div>
-    </footer>
-    <script src="assets/js/script.js"></script>
+        <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
+            <div class="movie-card">
+                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <h2><a href="pages/movie-detail.php">Movie Title</a></h2>
+            </div>
+        </div>
+        <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
+            <div class="movie-card">
+                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <h2><a href="pages/movie-detail.php">Movie Title</a></h2>
+            </div>
+        </div>
+        <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
+            <div class="movie-card">
+                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <h2><a href="pages/movie-detail.php">Movie Title</a></h2>
+            </div>
+        </div>
+        <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
+            <div class="movie-card">
+                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <h2><a href="pages/movie-detail.php">Movie Title</a></h2>
+            </div>
+        </div>
+        <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
+            <div class="movie-card">
+                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <h2><a href="pages/movie-detail.php">Movie Title</a></h2>
+            </div>
+        </div>
+        <div class="col-lg-4 col-xl-2 col-md-4 col-sm-6 col-xs-6 movie airing">
+            <div class="movie-card">
+                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <h2><a href="pages/movie-detail.php">Movie Title</a></h2>
+            </div>
+        </div>
+    </div>
+</main> -->
+
+<?php include 'includes/footer.php'; ?>
+<script src="assets/js/script.js"></script>
 </body>
 
 </html>
