35d993f373636426ccdc0a11c61d9ddaa6e09dcb
M Ridwan <ridwanpratama9984@gmail.com>
2023-11-07 10:25:55 +0700

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,18 @@
 <footer>
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <p>&copy; 2023 MovieFlix</p>
+        <div class="row">       
+            <div class="col-lg-4 col-xl-4 col-md-12 col-sm-12 col-xs-12 social left">
+                <h1 class="find-us">FIND US ON</h1>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-pinterest"></i>
+                <i class="fa-brands fa-youtube"></i>
+                <i class="fa-brands fa-linkedin"></i>
+            </div>
+            <div class="col-lg-8 col-xl-8 col-md-12 col-sm-12 col-xs-12 center">
+                <p class="term"> All materials and contents (text, graphics, and every attributes) of MovieFlix or MovieFlix.com website are copyrights and trademarks of MovieFlix. No part of this website may be reproduced in any form without our written permission. Misuse of the entire content or any part, multiply, translate, use, or utilize it without written permission from MovieFlix will be subject to criminal and / or civil penalties. admin@mflix-web-01 </p>
+                <p class="copyright">&copy; 2023 MovieFlix</p>
             </div>
         </div>
     </div>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/a3b5e10b5f.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="assets/css/reset.css">
     <link rel="stylesheet" href="assets/css/grid.css">
     <link rel="stylesheet" href="assets/css/style.css">
@@ -33,37 +33,37 @@
         </div>
         <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-6 movie airing">
             <div class="movie-card">
-                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <img src="assets/img/movie2.jpg" alt="Movie Title">
             </div>
         </div>
         <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-6 movie airing">
             <div class="movie-card">
-                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <img src="assets/img/movie3.jpg" alt="Movie Title">
             </div>
         </div>
         <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-6 movie airing">
             <div class="movie-card">
-                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <img src="assets/img/movie4.jpg" alt="Movie Title">
             </div>
         </div>
         <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-6 movie airing">
             <div class="movie-card">
-                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <img src="assets/img/movie5.jpg" alt="Movie Title">
             </div>
         </div>
         <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-6 movie airing">
             <div class="movie-card">
-                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <img src="assets/img/movie6.jpg" alt="Movie Title">
             </div>
         </div>
         <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-6 movie airing">
             <div class="movie-card">
-                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <img src="assets/img/movie7.jpg" alt="Movie Title">
             </div>
         </div>
         <div class="col-lg-3 col-xl-3 col-md-4 col-sm-6 col-xs-6 movie airing">
             <div class="movie-card">
-                <img src="assets/img/movie1.jpg" alt="Movie Title">
+                <img src="assets/img/movie8.jpg" alt="Movie Title">
             </div>
         </div>
       
